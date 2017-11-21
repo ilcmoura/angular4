23a8dcc0d8754be73f9ff85a56b4cfda6a4e6d63
Igor Louback <igor@ilit.com.br>
2017-11-21 11:31:40 -0300

    alterando o formulário para utilizar ReactiveForms

--- meat-app-starter/src/app/order/order.component.ts
+++ meat-app-starter/src/app/order/order.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
 import {Router} from '@angular/router'
 
 import {OrderService} from './order.service'
@@ -16,6 +18,12 @@ import {Order, OrderItem} from './order.model'
 })
 export class OrderComponent implements OnInit {
 
+  emailPattern =  /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+
+  numberPattern =  /^[0-9]*$/
+
+  orderForm: FormGroup
+
   paymentOptions: RadioOption [] = [
     {"label": "Dinheiro", "value": "DIN"},
     {"label": "Crédito", "value": "CRED"},
@@ -24,9 +32,19 @@ export class OrderComponent implements OnInit {
 
   delivery: number = 8
 
-  constructor(private orderService : OrderService, private router: Router) { }
+  constructor(private orderService : OrderService, private router: Router, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+      this.orderForm = this.formBuilder.group({
+        //name: '', //forma reduzida de representar a opção abaixo
+        name: this.formBuilder.control('', [Validators.required, Validators.minLength(5)]),
+        email: this.formBuilder.control('', [Validators.required, Validators.pattern(this.emailPattern)]),
+        emailConfirmation: this.formBuilder.control('', [Validators.required, Validators.pattern(this.emailPattern)]),
+        address: this.formBuilder.control('',[Validators.required, Validators.minLength(5)]),
+        number: this.formBuilder.control('',[Validators.required, Validators.pattern(this.numberPattern)]),
+        optionalAddress: this.formBuilder.control(''),
+        paymentOption: this.formBuilder.control('', [Validators.required])
+      })
   }
 
   itemsValue() :number {
